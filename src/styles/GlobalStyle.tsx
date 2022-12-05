21f3752788c39ed8be95f8e80628a5595097d31f
Joohyun Kim <corinthionia@gmail.com>
2022-12-05 16:41:41 +0900

    :lipstick:  design: add global style

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,44 @@
+import { Global, css } from '@emotion/react';
+
+const defaultStyle = css`
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
+      format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-SemiBold.woff')
+      format('woff');
+    font-weight: 600;
+    font-style: normal;
+  }
+
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Pretendard', Arial, Helvetica, sans-serif;
+  }
+
+  html,
+  body {
+    height: 100%;
+  }
+
+  a,
+  a:hover {
+    color: inherit;
+    text-decoration: none;
+    cursor: pointer;
+  }
+`;
+
+const GlobalStyle = function () {
+  return <Global styles={defaultStyle} />;
+};
+
+export default GlobalStyle;
