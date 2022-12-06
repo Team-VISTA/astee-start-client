d9dcb5b5ec2632f4a62d8304e7877376e850c105
Joohyun Kim <corinthionia@gmail.com>
2022-12-06 13:30:19 +0900

    :lipstick:  design: update header design

--- src/components/commons/Header.tsx
+++ src/components/commons/Header.tsx
@@ -1,14 +1,68 @@
 import styled from '@emotion/styled';
+import theme from '../../styles/theme';
 
 const Header = () => {
-  return <Wrapper>This is Header</Wrapper>;
+  return (
+    <Wrapper>
+      <Brand>
+        <Logo src="/images/astee.svg" />
+        <Astee>Astee</Astee>
+      </Brand>
+      <User>
+        <Span>로그인</Span>
+        <Border />
+        <Span>로그아웃</Span>
+      </User>
+    </Wrapper>
+  );
 };
 
 export default Header;
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 120px;
+  height: 10vh;
+  padding: 0 36px 0 16px;
 
   display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Brand = styled.div`
+  width: 108px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Logo = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+
+const Astee = styled.label`
+  color: ${theme.brand.main};
+  font-size: 28px;
+`;
+
+const User = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  font-size: 14px;
+  color: ${theme.colors.white};
+`;
+
+const Span = styled.span`
+  cursor: pointer;
+`;
+
+const Border = styled.div`
+  width: 1px;
+  height: 15px;
+  margin: 0 14px;
+  background: ${theme.colors.white};
 `;
